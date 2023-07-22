3b7aba9fc35a1eff18aa0cffe5fd8259f746a24b
Kade Satrya <kade.satrya@ui.ac.id>
2023-07-22 15:12:13 +0700

    Fix user authentication

--- authentication/views.py
+++ authentication/views.py
@@ -27,9 +27,9 @@ def register_user(request):
 
 def login_user(request):
     if (request.method == "POST"):
-        email = request.POST.get("email")
+        username = request.POST.get("username")
         password = request.POST.get("password")
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect("main:main")
