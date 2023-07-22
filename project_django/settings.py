9992711ae44b7c38d35bffeda129faf3931b6eb2
sherlockjack <sherlocknagata@gmail.com>
2023-07-22 23:59:28 +0700

    semoga berhasil

--- project_django/settings.py
+++ project_django/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example',
+    'authentication',
+    'donation',
+    'main',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
--- project_django/urls.py
+++ project_django/urls.py
@@ -1,31 +1,16 @@
-"""project_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+WSGI config for now_app project.
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('donation/', include('donation.urls')),
-    path('auth/', include('authentication.urls')),
-    path('profile/', include('user_profile.urls'))
-]
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
 
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+
+application = get_wsgi_application()
