783d339fa03adbdc491dccf2aef44549a08a57b7
Kade Satrya <kade.satrya@ui.ac.id>
2023-07-22 16:13:06 +0700

    fix user profile edit

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from authentication.models import User
+from django.contrib import messages
 
 @login_required
 def show_profile(request):
     current_user = request.user
-    context = {"user":current_user}
+    context = {"user" : current_user}
     return render(request, "profile.html", context)
 
 @login_required
@@ -13,16 +15,37 @@ def edit_profile(request):
     context = {"user":current_user}
     
     if (request.method == "PUT"):
-        current_user.username = request.PUT.get("username")
-        current_user.email = request.PUT.get("email")
-        current_user.password = request.PUT.get("password")
-        contact = request.PUT.get("contact").strip()
+        username = request.PUT.get("username")
+        email = request.PUT.get("email")
+        password = request.PUT.get("password")
         
-        if (contact == "" ):
-            current_user.contact = None
+        if (not validate_profile(current_user, username, email)):
+            messages.info(request, message_list[validate_profile])
+        
         else:
-            current_user.contact = contact
-
-        current_user.save()
+            contact = request.PUT.get("contact").strip()
+            if (contact == "" ):
+                current_user.contact = "-"
+            else:
+                current_user.contact = contact
+            current_user.username = username
+            current_user.email = request.PUT.get("email")
+            current_user.password = request.PUT.get("password")
+            current_user.save()
+            return redirect("user_profile:")
         
     return render(request, "edit_profile.html", context)
+
+# Utilities
+message_list = {
+    1:"Username already taken",
+    2:"Email already taken",        
+            }
+
+def validate_profile(current_user, username, email):
+    if (User.objects.filter(username=username) and current_user.username != username):
+        return 1
+    if (User.objects.filter(email=email) and current_user.email != email):
+        return 2
+    return 0
+    
