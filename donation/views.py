2670c559d58d099c2aba4689608718c2eb72f136
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2023-07-22 16:49:02 +0700

    add my donations, restrict edit to donator

--- donation/views.py
+++ donation/views.py
@@ -4,12 +4,25 @@ from django.shortcuts import redirect, render
 from donation.models import *
 
 def show_available_donations(request):
-    context = {"donations": Donation.objects.filter(status=True).order_by("-datetime_created")}
-    return render(request, "available_donations.html", context)
+    context = {
+        "donations": Donation.objects.filter(status=True).order_by("-datetime_created"),
+        "public": True
+    }
+    return render(request, "list_donations.html", context)
 
 @login_required(login_url="authentication:login")
+def show_my_donations(request):
+    context = {
+        "donations": Donation.objects.filter(donator=request.user).order_by("-datetime_created"),
+        "public": False
+    }
+    return render(request, "list_donations.html", context)
+
 def show_donation_details(request, id):
-    context = {"donation": Donation.objects.filter(pk=id)}
+    context = {
+        "donation": Donation.objects.get(pk=id),
+        "logged_user": request.user
+    }
     return render(request, "donation_details.html", context)
 
 @login_required(login_url="authentication:login")
@@ -56,7 +69,6 @@ def edit_existing_donation(request, id):
         weight_grams = int(request.POST["weight_grams"])
         address = request.POST["address"]
         description = request.POST["description"]
-        status = request.POST["status"]
         if len(title) > 100:
             context["title_warning"] = "Title must be no more than 100 characters long."
         if weight_grams <= 0:
@@ -70,9 +82,8 @@ def edit_existing_donation(request, id):
         edited_donation.weight_grams = weight_grams
         edited_donation.address = address
         edited_donation.description = description
-        edited_donation.status = status
         edited_donation.save()
         return redirect("donation:show_donation_details", id)
 
-    context["donation"] = Donation.objects.filter(pk=id)
+    context["donation"] = Donation.objects.get(pk=id)
     return render(request, "edit_donation.html", context)
